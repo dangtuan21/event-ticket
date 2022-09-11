13182cedb20fbeb9e4df841972ef8628b39f5f42
Tuan <dangtuan21@gmail.com>
2022-09-11 13:26:23 -0400

    rename files

--- EventMarketplace.sol
+++ EventMarketplace.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "./EventToken.sol";
+import "./EVENTToken.sol";
 import "hardhat/console.sol";
 
 contract EVENTMarketplace is Ownable
